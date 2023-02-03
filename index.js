4f01b538b9508b9973648d7d155a9ab9169cbd6d
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-02-03 16:55:36 -0500

    creating html

--- index.js
+++ index.js
@@ -1,52 +1,70 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 const questions = [
     {
     type: 'input',
-    name: 'title',
-    message: 'What is your projects title?',
+    name: 'name',
+    message: 'What is the employees name?',
   },
   {
     type: 'input',
-    name: 'description',
-    message: 'Enter a description of your project:',
-  },
-  {
-    type: 'input',
-    name: 'installation',
-    message: 'What needs to be installed?',
-  },
-  {
-    type: 'input',
-    name: 'usage',
-    message: 'Are there any usage requirements?',
-  },
-  {
-    type: 'list',
-    name: 'license',
-    message: 'What type of license does the project have?',
-    choices: ['MIT License', 'Apache License 2.0', 'BSD License', 'GNU GPLv3'],
-  },
-  {
-    type: 'input',
-    name: 'contribute',
-    message: 'How to contribute to this project:',
-  },
-  {
-    type: 'input',
-    name: 'tests',
-    message: 'What tests can be run?',
-  },
-  {
-    type: 'input',
-    name: 'github',
-    message: 'Enter your GitHub Username:',
+    name: 'id',
+    message: 'What is the employees id number?',
   },
   {
     type: 'input',
     name: 'email',
-    message: 'Enter your email:',
+    message: 'What is the employees email address?',
+  },
+  {
+    type: 'input',
+    name: 'officeNum',
+    message: 'What is the employees office number?',
   },
 ];
+
+const generateHTML = ({ name, id, email, officeNum, github}) =>
+  `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+      <link rel="stylesheet" href="./style.css">
+      <title>Document</title>
+  </head>
+  <body>
+      <header>
+          <h1 id="title">Title</h1>
+      </header>
+  
+      <main>
+  <!-- this section will have tiles that contain the employee information -->
+  
+          <div class="card" style="width: 18rem;">
+              <div class="card-body">
+              <h5 class="card-title">${name}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="card-link">Card link</a>
+              <a href="#" class="card-link">Another link</a>
+          </div>
+    </div>
+  
+      </main>
+  
+      <footer>
+          <p>footer information</p>
+      </footer>
+      
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  
+  </body>
+  </html>`
 
 function init() {
 
@@ -54,11 +72,13 @@ function init() {
   inquirer
     .prompt(questions)
       .then((answers) => {
-        const readMeContent = generateReadMe(answers);
+        const htmlContent = generateHTML(answers);
     
-        fs.writeFile('README2.md', readMeContent, (err) =>
-          err ? console.log(err) : console.log('Successfully created README.md!')
+        fs.writeFile('index.html', htmlContent, (err) =>
+          err ? console.log(err) : console.log('Successfully created index.html!')
         );
       });
 
 }
+
+init();
