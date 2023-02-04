bbd7dbd71fba03fe5ffa7e0bec0505079b8edf11
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-02-04 07:48:26 -0500

    page styling

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const generateHTML = ({ name, id, email, officeNum, github}) =>
       </main>
   
       <footer>
-          <p>footer information</p>
+          <p>Developed by Nick Herman | Github: <a href="https://github.com/nwherman1724">https://github.com/nwherman1724</a></p>
       </footer>
       
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
